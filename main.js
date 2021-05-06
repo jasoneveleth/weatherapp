31f015b18c9654dc4a0b2d3b2d75a1a265cbcc6b
Jason Eveleth <jj11@eveleth.com>
2021-05-06 18:29:49 -0400

    requires you to provide location, but works
        dates need to be changed

--- main.js
+++ main.js
@@ -1,74 +1,54 @@
-function requestlocation() {
-return new Promise( function(resolve, reject){
-    if (navigator.geolocation) {
-        resolve(navigator.geolocation.getCurrentPosition(getGridUrl));
-        console.log("got location")
-    } else { 
-	reject("Geolocation is not supported by this browser");
-        //alert("Geolocation is not supported by this browser.")
-  }
-  })	
-}
-
-document.addEventListener("DOMContentLoaded", requestlocation().then(loadChart));
-
-// url = 'https://api.weather.gov/points/42,-73'
-url = 'https://api.weather.gov/gridpoints/BOX/8,49'
-baseUrl = 'https://api.weather.gov/points/'
-function getGridUrl(position){
-    latitude=position.coords.latitude.toFixed(5);
-    longitude=position.coords.longitude.toFixed(5);
-    baseUrl += latitude + ',' + longitude;
-    console.log(baseUrl);
-    fetch(baseUrl)
-    .then(res => res.json())
-    .then((obj) => {
-        url = obj.properties.forecastGridData;
-        console.log(url);
-     })
-}
-
-
-
-
-const iso8601dateregex = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\+(\d{2}:\d{2})\/PT(\d)H/
-// year, month, day, hour, min, sec, offset (truncated down), interval (truncated to hours)
-// 1       2     3     4     5   6      7                       8
-
 function formatdate(str) {
-    const matches = str.match(iso8601dateregex)
-    const date = new Date(matches[1], matches[2], matches[3], matches[4], matches[5], matches[6], 0)
+    const excludeintervalregex = /^[-0-9T:+]+/
+    console.log(str.match(excludeintervalregex)[0])
+    console.log("new Date(\"" + str.match(excludeintervalregex)[0] + "\")")
+    const date = new Date(str.match(excludeintervalregex)[0])
     return date.toString()
 }
 
-function loadChart(){
-fetch(url)
-.then(res => res.json())
-.then((obj) => {
-    const skyCover = obj.properties.skyCover.values
-    console.log(skyCover)
+function loadChart(url) {
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then((obj) => {
+            const skyCover = obj.properties.skyCover.values
 
-    const times = skyCover.reduce((acc, val) =>  acc.concat([formatdate(val.validTime)]), [])
-    const percentages = skyCover.reduce((acc, val) =>  acc.concat(val.value), [])
+            const times = skyCover.reduce((acc, val) =>  acc.concat([formatdate(val.validTime)]), [])
+            const percentages = skyCover.reduce((acc, val) =>  acc.concat(val.value), [])
 
-    const data = { 
-        labels: times, 
-        datasets: [{
-            label: 'Sky cover',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: percentages,
-        }] 
-    }
+            const data = { 
+                labels: times, 
+                datasets: [{
+                    label: 'Sky cover',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: percentages,
+                }] 
+            }
 
-    const config = {
-        type: 'line',
-        data,
-        options: {}
-    }
-    var myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
-})
+            const config = {
+                type: 'line',
+                data,
+                options: {}
+            }
+            var myChart = new Chart(
+                document.getElementById('myChart'),
+                config
+            );
+        })
 }
+
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position)
+    const baseUrl = 'https://api.weather.gov/points/'
+    latitude=position.coords.latitude.toFixed(5);
+    longitude=position.coords.longitude.toFixed(5);
+    const url = baseUrl + latitude + ',' + longitude;
+    // const url = "https://api.weather.gov/gridpoints/BOX/8,49"
+    fetch(url)
+        .then(res => res.json())
+        .then(obj => obj.properties.forecastGridData)
+        .then(url => loadChart(url))
+}
+)
+
